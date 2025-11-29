67a2336f4aadf44fc5622b84ca67695c1c26ac3f
sateesh3901 <yenugantisateesh777@gmail.com>
2025-11-29 23:30:50 +0530

    added requirements file

--- apicalls/settings.py
+++ apicalls/settings.py
@@ -26,9 +26,9 @@ env.read_env(os.path.join(BASE_DIR,'.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
